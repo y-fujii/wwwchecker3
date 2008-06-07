50a82ac6d26fb31079ac105dada92646ed8d8498
y-fujii <y-fujii@laptop>
2008-06-07 22:24:59 +0900

    Switch to BeautifulSoup

---
+++ parallel.py
@@ -0,0 +1,32 @@
+import sys
+import threading
+import Queue
+
+
+
+def run( procs, n ):
+	queue = Queue.Queue()
+	for proc in procs:
+		queue.put( proc )
+
+	thrs = []
+	for _ in xrange( n ):
+		def threadProc():
+			while True:
+				try:
+					proc = queue.get_nowait()
+				except Queue.Empty:
+					break
+
+				try:
+					proc()
+				except Exception, err:
+					sys.stderr.write( str( err ) + "\n" )
+					sys.stderr.flush()
+
+		thr = threading.Thread( target = threadProc )
+		thr.start()
+		thrs.append( thr )
+	
+	for thr in thrs:
+		thr.join()
