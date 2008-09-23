4d6871c3989f7a32e74cd365b772dd8cde0b0199
y-fujii <y-fujii@sunroom>
2008-09-23 18:47:41 +0900

    Finish w/o w3m rewriting

--- test.py
+++ test.py
@@ -1,8 +1,26 @@
 
+import pprint
 import urllib
 import htmlTools
 
-x = urllib.urlopen( "http://www.google.com/" ).read()
+src = """\
+<html>
+	<body>
+		<table>
+			<tr>
+				<td>A</td>
+				<td>B</td>
+			</tr>
+			<tr>
+				<td>C</td>
+				<td>D</td>
+			</tr>
+		</table>
+	</body>
+</html>
+"""
 
-for line in htmlTools.getContent( x )[1]:
-	print line.encode( "shift-jis", "ignore" )
+src = urllib.urlopen( "http://d.hatena.ne.jp/KZR/" ).read()
+
+for line in htmlTools.getContent( src )[0]:
+	print line.encode( "euc-jp", "ignore" )
